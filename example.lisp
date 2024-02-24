79fc026917fb895fb2a9183a420f9f211bffd84a
Homo-carbonis <homocarbonis@protonmail.com>
2024-02-24 10:33:24 +0000

    working inference

---
+++ example.lisp
@@ -0,0 +1,47 @@
+(defpackage :example
+  (:use :cl :infr))
+(defun example (beta ti) (exp (+ (* (first-elt beta) ti) (log 0.5))))
+
+(defdf data (generate-noisy (curry #'example #(0.001)) 1 100))
+
+(defun plotdata ()
+ (plot:plot
+  (vega:defplot simple-line-plot
+    `(:title "Noisy y data"
+      :data (:values ,data)
+      :mark :line
+      :encoding (:x (:field :time
+                     :type  :quantitative)
+                 :y (:field :value
+                     :type  :quantitative))))))
+ 
+(defun post (beta sigma^2)
+ (posterior (make-normal-pdf-over-f #'example)
+           (normal-pdf 0.1 0.0 1.0) (vector beta sigma^2) (rows data)))
+
+(plot:plot
+    (vega:defplot simple-line-plot
+      `(:title "Noisy y data"
+        :data (:values ,data)
+        :mark :line
+        :encoding (:x (:field :value
+                       :type  :quantitative)
+                   :y (:field :time
+                       :type  :quantitative)))))
+
+(let* ((beta* (make-array 2000 :element-type 'double-float))
+      (p* (make-array 2000 :element-type 'double-float))
+      (_ (loop for beta from -0.01d0 below 0.01d0 by 0.0001d0
+              for i upfrom 0
+              do (setf (aref beta* i) beta) 
+              do (setf (aref p* i) (post beta 1)) ))
+      (beta-dist (make-df '(:beta :p) (list beta* p*))))
+  (plot:plot
+    (vega:defplot simple-line-plot
+      `(:title "Noisy y data"
+        :data (:values ,beta-dist)
+        :mark :line
+        :encoding (:x (:field :beta
+                       :type  :quantitative)
+                   :y (:field :p
+                       :type  :quantitative))))) )
--- inference.lisp
+++ inference.lisp
@@ -4,7 +4,7 @@
 (require 'drom/raster)
 
 (defpackage :infr
-  (:use :cl :utils/misc :lisp-stat :plot :num-utils))
+  (:use :cl :utils/misc :lisp-stat :plot :num-utils :drom/raster :drom/interval))
 
 (in-package :infr)
 (defparameter plot:*default-browser-command* :chrome-app-mode)
@@ -18,59 +18,26 @@
           do (setf (aref y i) (draw (r-normal (funcall f i) variance))))
     (make-df '(:time :value) (list ti y))))
 
-(defun example (ti beta) (exp (+ (* beta ti) (log 0.5))))
 
-(defdf data (generate-noisy (rcurry #'example 0.001) 1 100))
-
-(defun plotdata ()
- (plot:plot
-  (vega:defplot simple-line-plot
-    `(:title "Noisy y data"
-      :data (:values ,data)
-      :mark :line
-      :encoding (:x (:field :time
-                     :type  :quantitative)
-                 :y (:field :value
-                     :type  :quantitative))))))
-
-(defun quadrature-2 (f interval-1 interval-2 &optional (epsilon 0.001))
-  (loop for i from (num-utils:left interval-1) below (num-utils:right interval-1) by epsilon
-        sum (loop for j from (num-utils:left interval-2) below (num-utils:right interval-2) by epsilon
-              sum (* epsilon (funcall f i j)))))
-
-(defun posterior (pdf prior parameters)
-  (let ((marginal (quadrature-2 pdf (interval 0.5 1.5) (interval -1.0 1.0)))
-        (likelihood (apply pdf parameters)))
-              (format t "prior: ~a, likelihood: ~a, marginal: ~a~%" prior likelihood marginal)
-              (/ (* likelihood prior) marginal))
-  )
-
-(defun posterior (pdf prior data* parameters)
-  (product a )
+(defun posterior (pdf prior parameters data*)
   (reduce (lambda (prior data)
-            (let* ((pdf (curry pdf data))
-                   (marginal (quadrature-2 pdf (interval 0.5 1.5) (interval -1.0 1.0)))
-                   (likelihood (apply pdf parameters)))
-              (format t "prior: ~a, likelihood: ~a, marginal: ~a~%" prior likelihood marginal)
+            (let* ((marginal 1)
+                   (likelihood (funcall pdf parameters data)))
               (/ (* likelihood prior) marginal)))
           data*
           :initial-value prior))
 
-(defun posterior (pdf prior data* parameters)
-  (reduce (lambda (prior data)
-            (let* ((pdf (curry pdf data))
-                   (marginal (quadrature-2 pdf (interval 0.5 1.5) (interval -1.0 1.0)))
-                   (likelihood (apply pdf parameters)))
-              (format t "prior: ~a, likelihood: ~a, marginal: ~a~%" prior likelihood marginal)
-              (/ (* likelihood prior) marginal)))
-          data*
-          :initial-value prior))
-
-(defun make-normal-over-f (f)
-  (lambda (data sigma^2 &rest parameters)
+(defun make-normal-pdf-over-f (f)
+  (lambda (parameters data)
     (let ((x (elt data 0))
-          (y (elt data 1)))
-    (normal-pdf y (apply f x parameters) sigma^2))))
+          (y (elt data 1))
+          (sigma^2 (last-elt parameters))
+          (parameters (subseq parameters 0 (1- (length parameters))))) 
+    (normal-pdf y (funcall f parameters x) sigma^2))))
 
-(posterior (make-normal-over-f #'example)
-           (normal-pdf 0.1 0.0 1.0) (rows data) '(1 0.001))
+
+
+
+
+
+
