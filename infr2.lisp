49e693b7666b14d57df85042f042313f2d2848cf
Homo-carbonis <homocarbonis@protonmail.com>
2024-02-27 16:29:25 +0000

    monte carlo estimate

--- infr2.lisp
+++ infr2.lisp
@@ -9,7 +9,7 @@
   (:shadowing-import-from :array-operations :generate)
   (:shadowing-import-from :iter :next :sum)
   (:import-from :serapeum :nlet)
-  (:use :cl :utils/misc :lisp-stat :iter :plot :num-utils))
+  (:use :cl :utils/misc :lisp-stat :iter :num-utils))
 
 (in-package :infr2)
 
@@ -19,11 +19,14 @@
             (for i below n)
             (setf (aref this i) x))))
 
-(defun simulate-wiener (dx/dt n &key (xi 0d0) (var 1d0))
+(defun simulate (step-x n &key (xi 0d0))
   (on (make-array n :element-type 'double-float :fill-pointer t)
-      (iter (for x first xi then (+ x (funcall dx/dt x) (draw (r-normal 0d0 var))))
+      (iter (for x first xi then (funcall step-x x))
             (for i below n)
             (setf (aref this i) x))))
+
+(defun step-wiener (dx/dt x &key (var 1d0))
+  (+ x (funcall dx/dt x) (draw (r-normal 0d0 var))))
 
 (defun wiener-pdf (dx/dt y &key (var 1d0))
   (normal-pdf (elmt y -1) (+ (elmt y -2) (funcall dx/dt (elmt y -2))) var))
@@ -42,8 +45,24 @@
         (format t "y: ~a, marginal: ~a, post: ~a~%" (elt y i) marginal posterior)
         (finally (return posterior))))
 
+(defun expected (model prior y &optional (offset 0))
+  (mean (iter (with beta* = (generate (curry #'draw prior) 100))
+              (for i from offset below (length y))
+              (setf (fill-pointer y) i)
+              (collect (iter (for beta in-vector beta*)
+                             (for y^ = (mean (generate (curry model beta y) 100)))
+                             (collect (/ 1 (square (- (aref y i) y^))) into weights)
+                             (finally (return (nu.statistics:mean beta* :weights weights))))))))
+;; lisp-stat:mean ignores weights if vector isn't just the right type
 
-
+(defun my-expected (beta)
+  (let* ((dx/dt (lambda (beta x) (* beta x)))
+        (y* (simulate (curry #'step-wiener (curry dx/dt beta)) 100))
+        (model (lambda (beta y) (step-wiener (curry dx/dt beta) (last-elt y))))
+        (prior (r-normal)))
+    #++(vgplot:plot y*)
+    (expected model prior y* 1)))
+(my-expected 0.1d0 )
 
 (defun my-posterior (beta beta^)
   (let* ((dx/dt (lambda (beta x) (* beta x)))
