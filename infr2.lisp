4577623a5693a191d2e5b6c5a53db63967246c85
Homo-carbonis <homocarbonis@protonmail.com>
2024-02-28 10:33:33 +0000

    Finally working

--- infr2.lisp
+++ infr2.lisp
@@ -7,7 +7,6 @@
 (defpackage :infr2
   (:shadowing-import-from :num-utils :product :left :right)
   (:shadowing-import-from :array-operations :generate)
-  (:shadowing-import-from :iter :next :sum)
   (:import-from :serapeum :nlet)
   (:use :cl :utils/misc :lisp-stat :iter :num-utils))
 
@@ -35,55 +34,43 @@
   (/ (* (pdf y (funcall likelihood beta)) (pdf beta prior))
      (funcall marginal likelihood prior y)))
 
-(defun posterior* (model prior beta y &optional (offset 0))
-  (iter (with beta* = (generate (curry #'draw prior) 1000))
-        (for i from offset below (length y))
-        (setf (fill-pointer y) (1+ i))
-        (for likelihood = (funcall model beta y))
-        (for marginal = (mean (map-array beta* (lambda (beta) (funcall model beta y)))))
-        (reducing (/ likelihood marginal) by #'* initial-value (pdf prior beta) into posterior)
-        (format t "y: ~a, marginal: ~a, post: ~a~%" (elt y i) marginal posterior)
-        (finally (return posterior))))
-
-(defun expected (model prior y &optional (offset 0))
-  (mean (iter (with beta* = (generate (curry #'draw prior) 100))
-              (for i from offset below (length y))
-              (setf (fill-pointer y) i)
-              (collect (iter (for beta in-vector beta*)
-                             (for y^ = (mean (generate (curry model beta y) 100)))
-                             (collect (/ 1 (square (- (aref y i) y^))) into weights)
-                             (finally (return (nu.statistics:mean beta* :weights weights))))))))
-;; lisp-stat:mean ignores weights if vector isn't just the right type
-
-(defun posterior% (model prior y &optional (offset 0))
-  (mean (iter (with beta* = (generate (curry #'draw prior) 100))
+; TODO: write mean reduction for iterate.
+(defun expected (model prior y &key (offset 0))
+  (mean (iter (with beta* = (generate prior 100))
               (for i from offset below (length y))
               (setf (fill-pointer y) i)
               (for weights = (iter (for beta in-vector beta*)
-                                   (for y^ = (mean (generate (curry model beta y) 100)))
-                                   (collect (/ 1 (square (- (aref y i) y^))))))
-              for beta^ = (mean beta* :weights weights)
-              (map-array beta* (/ 1 (square (- (aref beta* i) beta^))))
-              (collect beta^)
-              )))
+                             (for y^ = (mean (generate (curry model beta y) 100)))
+                             (collect (/ 1 (square (- (aref y i) y^))))))
+              ; Use nu.statistics:mean because lisp-stat:mean ignores weights
+              ; if vector isn't just the right type.
+              (collect (nu.statistics:mean beta* :weights weights)))))
+
+(defun model-likelihood (model y &key (offset 0))
+  (mean (iter (for i from offset below (length y))
+              (setf (fill-pointer y) i)
+              (for samples = (generate (curry model y) 100))
+              (collect (normal-pdf (aref y i) (mean samples) (sd samples))))))
+
+(defun posteriors (models priors y &key (offset 0))
+  (let* ((combined (map 'vector (lambda (m p) (* p (model-likelihood m y :offset offset))) models priors))
+        (marginal (sum combined)))
+    (map 'vector (lambda (c) (/ c marginal)) combined)))
 
 (defun my-expected (beta)
   (let* ((dx/dt (lambda (beta x) (* beta x)))
         (y* (simulate (curry #'step-wiener (curry dx/dt beta)) 100))
         (model (lambda (beta y) (step-wiener (curry dx/dt beta) (last-elt y))))
-        (prior (r-normal)))
+        (prior (curry #'draw (r-normal))))
     #++(vgplot:plot y*)
-    (expected model prior y* 1)))
+    (expected model prior y* :offset 1)))
+
 (my-expected 0.1d0 )
 
-(defun my-posterior (beta beta^)
+(defun my-post (beta)
   (let* ((dx/dt (lambda (beta x) (* beta x)))
-        (y* (simulate-wiener (curry dx/dt beta) 100))
-        (model (lambda (beta y) (wiener-pdf (curry dx/dt beta) y)))
-        (prior (r-normal)))
-    (vgplot:plot y*)
-    (posterior* model prior beta^ y* 1)))
-
-(my-posterior -0.1d0 -0.1d0)
-
-
+         (y* (simulate (curry #'step-wiener (curry dx/dt beta)) 100))
+         (model (lambda (beta y) (step-wiener (curry dx/dt beta) (last-elt y)))))
+    #++(vgplot:plot y*)
+    (posteriors (vector (curry model 0.0) (curry model 0.1) (curry model 0.2)) #(1/3 1/3 1/3) y* :offset 1)))
+(my-post 0.1)
